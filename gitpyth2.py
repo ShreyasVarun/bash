e0d77d1bceeeddde857eeef6def71dc9d9e4369b
varaj2523 <48651495+varaj2523@users.noreply.github.com>
2020-11-25 07:25:30 +0530

    Add files via upload

---
+++ gitpyth2.py
@@ -0,0 +1,14 @@
+import smtplib
+from datetime import datetime
+my_email = "vython2020@gmail.com"
+password = "optionchain"
+with open('/home/varun/Documents/projects/mobile/ip.txt','r') as ip:
+    #text = ip.read()
+    text = "the github/bash/python loop is complete. jai guru dev"
+    print(text, type(text))
+    connection = smtplib.SMTP("smtp.gmail.com",587)
+    connection.starttls()
+    connection.login(user=my_email,password=password)
+    connection.sendmail(
+from_addr=my_email,to_addrs="x16varunk@iima.ac.in",msg="Subject:Ip address\n\n"+str(datetime.now())+"\n"+text)
+    connection.close()
