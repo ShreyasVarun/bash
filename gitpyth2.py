44f1b542ffa39ec93e6430fb6ed9960f4c3d9c8c
varaj2523 <48651495+varaj2523@users.noreply.github.com>
2020-12-07 00:44:54 +0530

    Update gitpyth2.py

--- gitpyth2.py
+++ gitpyth2.py
@@ -1,5 +1,12 @@
+import os
+os.system("pip3 install bs4")
+
 import smtplib
 from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+
+
 def send_email(text):
     my_email = "vython2020@gmail.com"
     password = "optionchain"
@@ -16,9 +23,14 @@ def send_email(text):
     connection.starttls()
     connection.login(user=my_email,password=password)
     connection.sendmail(
-    from_addr=my_email,to_addrs="x16varunk@iima.ac.in",msg="Subject:automation update\n\n" + "this mail was sent at " + str(datetime.now())+"\n"+text)
+    from_addr=my_email,to_addrs="x16varunk@iima.ac.in",msg="Subject:automation update\n\n" + "this mail was sent at " + str(datetime.now())+"\n"+text2)
     connection.close()
-text = f"single .sh file, send_email function, From continental@ 29-nov-2020,21:12 am   jai guru dev. "
-send_email(text)
+text2 = f"single .sh file, send_email function, From aig@ 06-nov-2020,21:12 am   jai guru dev.\n "
+
+reqs = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+#print (reqs.text)
+soup2 = BeautifulSoup(reqs.text,"html.parser")
+text2 += soup2.find_all("h3")[numpy.random.randint(1,100)].text
+send_email(text2)
 
     
