0732ab4be8ec6db2841d9d25031566bd4d1c4469
varaj2523 <48651495+varaj2523@users.noreply.github.com>
2020-12-12 10:43:15 +0530

    Update gitpyth2.py

--- gitpyth2.py
+++ gitpyth2.py
@@ -5,28 +5,18 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def send_email(text):
+def send_email(msg_body):
     my_email = "vython2020@gmail.com"
-    password = "optionchain"
-    #with open('/home/varun/Documents/projects/mobile/ip.txt','r') as ip:
-        #text = ip.read()
-    #text = "automation done by single .sh file that will update the repo and run this python file. From myhome@ 29-nov-2020,11:05 am   jai guru dev. "
-    try:
-        import tensorflow as tf
-        text += str(tf.__version__)
-    except Exception as e:
-        text += str(e)
-    #print(text, type(text))
+    password = "optionchain"   
     connection = smtplib.SMTP("smtp.gmail.com",587)
     connection.starttls()
     connection.login(user=my_email,password=password)
     connection.sendmail(
-    from_addr=my_email,to_addrs="x16varunk@iima.ac.in",msg="Subject:automation update\n\n" + "this mail was sent at " + str(datetime.now())+"\n"+text2)
+    from_addr=my_email,to_addrs="x16varunk@iima.ac.in",msg="Subject:automation update\n\n" + "this mail was sent at " + str(datetime.now())+"\n"+msg_body)
     connection.close()
+    
 text2 = f"jai guru dev\npull is working.\nsh file is working\ncrontab is working. 12-dec-2020 9:45 @mhj\n "
-
 reqs = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
-#print (reqs.text)
 soup2 = BeautifulSoup(reqs.text,"html.parser")
 text2 += soup2.find_all("h3")[numpy.random.randint(1,100)].text
 send_email(text2)
