808756a18c7bd352f19172b91f89f081b8236e4d
ShreyasVarun <x16varunk@iima.ac.in>
2021-01-02 15:12:30 +0530

    Enter

--- saturday.py
+++ saturday.py
@@ -1,3 +1,7 @@
 'jai guru dev'
 from datetime import datetime
-print(f' this is from SATURDAY {datetime.today()}')
+def main():
+    print(f' this is from SATURDAY {datetime.today()}')
+
+if __name__ == '__main__':
+    main()
