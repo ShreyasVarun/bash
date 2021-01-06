997f2e86129fa978e07a4b5bf0e8bc02dd0e984a
ShreyasVarun <x16varunk@iima.ac.in>
2021-01-06 18:59:28 +0530

    name

--- gitpyth2.py
+++ gitpyth2.py
@@ -14,11 +14,14 @@ def send_email(msg_body):
     connection.sendmail(
     from_addr=my_email,to_addrs="x16varunk@iima.ac.in",msg="Subject:automation update\n\n" + "this mail was sent at " + str(datetime.now())+"\n"+msg_body)
     connection.close()
-    
-text2 = f"jai guru dev\npull is working.\nsh file is working\ncrontab is working. 12-dec-2020 9:45 @mhj\n "
-reqs = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
-soup2 = BeautifulSoup(reqs.text,"html.parser")
-text2 += soup2.find_all("h3")[numpy.random.randint(1,100)].text
-send_email(text2)
+def main():
+    text2 = f"jai guru dev\npull is working.\nsh file is working\ncrontab is working. 12-dec-2020 9:45 @mhj\n "
+    reqs = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+    soup2 = BeautifulSoup(reqs.text,"html.parser")
+    text2 += soup2.find_all("h3")[numpy.random.randint(1,100)].text
+    send_email(text2)
+
+if __name__ == '__main__':
+    main()
 
     
