32c7dcd681bededfd05d512a1c8f7dc41797b54e
ShreyasVarun <x16varunk@iima.ac.in>
2021-01-06 18:43:48 +0530

    import

--- wednesday.py
+++ wednesday.py
@@ -1,3 +1,5 @@
 'jai guru dev'
+from gitpyth2 import send_email
 from datetime import datetime
-print(f' this is from WEDNEDSDAY {datetime.today()}')
+
+send_email(f' this is from WEDNEDSDAY {datetime.today()}, continental and I have used "from gitpyth2 import send_email" ')
