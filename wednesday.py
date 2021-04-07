f30e577315f2df501b486b948601f8ef24fa4e08
ShreyasVarun <x16varunk@iima.ac.in>
2021-04-07 12:38:44 +0530

    ok

--- wednesday.py
+++ wednesday.py
@@ -2,5 +2,9 @@
 from gitpyth2 import send_email
 from datetime import datetime
 
-send_email(f' latest update is that, i m typing this mail from bhaskar\
-     on {datetime.now()}  ')
+def main():
+     send_email(f' latest update is that, i m typing this mail from bhaskar\
+          on {datetime.now()}  ')
+if __name__ == '__main__':
+     pass
+     main()
